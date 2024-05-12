d167c7183a96bbd4666744a87c8e7625e91ac215
OliverToft <olivertoftk@live.dk>
2024-05-12 12:40:42 +0200

    we go agane

--- ast.ml
+++
@@ -1,8 +0,0 @@
-
-type expr =
-  | Int of int
-  | Var of string
-  | Add of expr * expr
-  | Sub of expr * expr
-  | Mul of expr * expr
-  | Div of expr * expr
--- evaluator.ml
+++
@@ -1,8 +0,0 @@
-
-let rec eval = function
-  | Ast.Int i -> i
-  | Ast.Var v -> failwith ("Unbound variable: " ^ v)
-  | Ast.Add (e1, e2) -> eval e1 + eval e2
-  | Ast.Sub (e1, e2) -> eval e1 - eval e2
-  | Ast.Mul (e1, e2) -> eval e1 * eval e2
-  | Ast.Div (e1, e2) -> eval e1 / eval e2
