4ef398c156299e0b8d819a2264e2813494e2f668
OliverToft <olivertoftk@live.dk>
2024-05-12 17:01:19 +0200

    removed tests

--- test/test_compiler.ml
+++
@@ -1,9 +0,0 @@
-let test_cases = [ "1 + 2"; "10 - 5"; "2 * 3"; "10 / 2" ]
-
-let () =
-  List.iter
-    (fun expr ->
-      let result = Compiler.Compile.compile_and_evaulate expr in
-      Printf.printf "%s -> %s\n" expr result)
-    test_cases
-;;
